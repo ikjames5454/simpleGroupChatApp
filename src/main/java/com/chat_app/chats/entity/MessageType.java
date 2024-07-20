45143be04e98e5ad36fba07b488b0c9daeb1ca11
ikjames5454 <ikennajames03@gmail.com>
2024-07-20 23:14:16 +0100

    add

---
+++ src/main/java/com/chat_app/chats/entity/MessageType.java
@@ -0,0 +1,7 @@
+package com.chat_app.chats.entity;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE,
+}
---
+++ src/test/java/com/chat_app/chats/ChatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_app.chats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
